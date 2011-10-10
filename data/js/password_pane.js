c5159b526dabad7cac3735f4f1f1158511e98a6d
Paul Sawaya <me@paulsawaya.com>
2011-10-10 16:41:16 -0400

    beginning of new popup alerts

--- data/js/password_pane.js
+++ data/js/password_pane.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         for (var login in msg.loginTable) {
             var httpsOnly = true;
             for (var hostname in msg.loginTable[login]) {
-                if (msg.loginTable[login][hostname].substr(0,8) != 'https://') {
+                if (msg.loginTable[login][hostname].hostname.substr(0,8) != 'https://') {
                     httpsOnly = false;
                     break;
                 }
@@ -18,8 +18,11 @@ $(document).ready(function() {
 
             newPasswordDiv.children('.passwordCleartext').html(login);
 
-            newPasswordDiv.children('.passwordHash').css('background-image',gradientStringForHash(SHA1(login)));
-            newPasswordDiv.find('.passwordSites').html(msg.loginTable[login].join(','));
+            newPasswordDiv.children('.passwordHash').css('background-color','');
+            newPasswordDiv.children('.passwordHash').css('background-image','url(' + getDataURLForHash(SHA1(login),200,25) + ')');
+            
+            var justHosts = msg.loginTable[login].map(function(x) { return x.host; });
+            newPasswordDiv.find('.passwordSites').html(justHosts.join(', '));
 
             $('#passwords').append(newPasswordDiv);
             newPasswordDiv.show();
@@ -29,6 +32,25 @@ $(document).ready(function() {
             self.postMessage({
                 type: 'typePassword',
                 password: $(this).children('.passwordCleartext').html()
+            });
+        });
+        
+        $('.passwordDiv').mouseout(function() {
+           $(this).children('.passwordHash').stop();
+           $(this).children('.passwordHash').html('');
+        });
+        
+        $('.passwordHash').mouseenter(function() {
+            var passwordHash = this;
+            $(passwordHash).animate({
+                opacity: 0.0
+            },3000, function() {
+                $(passwordHash).html("(your password here)");
+                // $(passwordHash).html($(passwordHash).parent().children('.passwordCleartext').html());
+                $(passwordHash).css('background-image','');
+                $(passwordHash).css('background-color','#000000');
+                $(passwordHash).css('color','#ffffff');
+                $(passwordHash).css('opacity',1);
             });
         });
     });    
