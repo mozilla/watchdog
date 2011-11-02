ed00b95d30054a3a9179e7afa2699858645da365
Paul Sawaya <me@paulsawaya.com>
2011-11-02 17:24:27 -0400

    password pane sorts by number of sites a login is used on

--- data/js/password_pane.js
+++ data/js/password_pane.js
@@ -2,10 +2,22 @@ $(document).ready(function() {
     self.port.on('loginData', function(msg){
         // First, clear everything.
         $('#passwords').html('');
+        
+        var loginList = [];
         for (var login in msg.loginTable) {
+            loginList.push({
+                password: login,
+                sites: msg.loginTable[login]
+            });
+        }
+        
+        // Sort passwords by number of sites they're used on.
+        loginList.sort(function(a,b) { return b.sites.length - a.sites.length; });
+        
+        for (var login in loginList) {
             var httpsOnly = true;
-            for (var hostname in msg.loginTable[login]) {
-                if (msg.loginTable[login][hostname].hostname.substr(0,8) != 'https://') {
+            for (var hostname in loginList[login].sites) {
+                if (loginList[login].sites[hostname].hostname.substr(0,8) != 'https://') {
                     httpsOnly = false;
                     break;
                 }
@@ -16,15 +28,19 @@ $(document).ready(function() {
             if ((msg.url.substr(0,8) == 'https://') != httpsOnly)
                 newPasswordDiv.addClass('notRecommended');
 
-            newPasswordDiv.children('.passwordCleartext').html(login);
+            newPasswordDiv.children('.passwordCleartext').html(loginList[login].password);
 
-            newPasswordDiv.children('.passwordHash').css('background-image','url(' + getDataURLForHash(SHA1(login),200,25) + ')');
+            newPasswordDiv.children('.passwordHash').css('background-image','url(' + getDataURLForHash(SHA1(loginList[login].password),200,25) + ')');
             
-            var justHosts = msg.loginTable[login].map(function(x) { return x.host; });
+            var justHosts = loginList[login].sites.map(function(x) { return x.host; });
             newPasswordDiv.find('.passwordSites').html(justHosts.join(', '));
 
             $('#passwords').append(newPasswordDiv);
             newPasswordDiv.show();
+        }
+        
+        function disappearWarning(passwordHash) {
+            $(passwordHash).parent().children(".passwordWarning").remove();
         }
         
         $('.passwordDiv').click(function() {
@@ -38,11 +54,19 @@ $(document).ready(function() {
             var clearTextPassword = $(this).children('.passwordCleartext').html();
             $(this).children('.passwordHash').stop();
             $(this).children('.passwordHash').html('');
+            $(this).children('.passwordHash').css('opacity',1);
             $(this).children('.passwordHash').css('background-image','url(' + getDataURLForHash(SHA1(clearTextPassword),200,25) + ')');
+            disappearWarning($(this).children('.passwordHash'));
         });
         
         $('.passwordHash').mouseenter(function() {
             var passwordHash = this;
+            
+            var passwordWarning = $("<div>About to display password...</div>");
+            passwordWarning.addClass('passwordWarning');
+            
+            $(passwordHash).parent().append(passwordWarning);
+            
             $(passwordHash).animate({
                 opacity: 0.0
             },3000, function() {
@@ -52,6 +76,7 @@ $(document).ready(function() {
                 $(passwordHash).css('background-color','#000000');
                 $(passwordHash).css('color','#ffffff');
                 $(passwordHash).css('opacity',1);
+                disappearWarning(passwordHash);
             });
         });
     });    
