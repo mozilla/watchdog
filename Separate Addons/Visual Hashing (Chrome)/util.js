d6b70d42af1bb58a5f94dc5f8784f7fefd5ccda9
Paul Sawaya <me@paulsawaya.com>
2011-11-13 16:10:42 -0500

    fixed (hopefully) one last bug in visual hashing, removed symlinks from separate addons.

--- Separate Addons/Visual Hashing (Chrome)/util.js
+++ Separate Addons/Visual Hashing (Chrome)/util.js
@@ -61,11 +61,13 @@ function getDataURLForHash(passwordHash,inputWidth,inputHeight) {
     for (var byteIdx = 0; byteIdx < passwordHash.length/2; byteIdx++) {
         var byte = parseInt(passwordHash.substr(byteIdx*2,2),16);
         // +/- 3, within 0-255
-        byte = Math.min(Math.max(byte + parseInt(Math.random()*6)-3,0),255); 
-        passwordHash = passwordHash.substr(0,byteIdx*2) + byte.toString(16) + passwordHash.substr(byteIdx*2+2);
+        byte = Math.min(Math.max(byte + parseInt(Math.random()*6)-3,0),255);
+        var hexStr = byte.toString(16).length == 2 ? byte.toString(16) : '0' + byte.toString(16);
+        passwordHash = passwordHash.substr(0,byteIdx*2) + hexStr + passwordHash.substr(byteIdx*2+2);
     }
-    
+
     for (var hashBandX = 0; hashBandX < 4; hashBandX++) {
+        console.log("Color " + hashBandX + " : " + '#' + passwordHash.substr(hashBandX*6,6));
         context.fillStyle='#' + passwordHash.substr(hashBandX*6,6);
         context.fillRect(hashBandX/4*inputWidth,0,inputWidth/4,inputHeight);
         
---
+++ copy_to_separate_addons.sh
@@ -0,0 +1,18 @@
+# Visual hashing (Jetpack)
+cp data/js/util.js Separate\ Addons/Visual\ Hashing/data/js/util.js
+cp data/js/visualhash.js Separate\ Addons/Visual\ Hashing/data/js/visualhash.js
+
+# Reuse Visualizer
+cp data/lock_blue.png Separate\ Addons/Reuse\ Visualizer/data/lock_blue.png
+cp data/view_passwords.html Separate\ Addons/Reuse\ Visualizer/data/view_passwords.html
+cp data/js/visualhash.js Separate\ Addons/Reuse\ Visualizer/data/js/visualhash.js
+cp data/js/d3* Separate\ Addons/Reuse\ Visualizer/data/js/
+cp data/js/util.js Separate\ Addons/Reuse\ Visualizer/data/js/util.js
+cp data/js/view_passwords_content_script.js Separate\ Addons/Reuse\ Visualizer/data/js/view_passwords_content_script.js
+cp data/js/view_passwords.js Separate\ Addons/Reuse\ Visualizer/data/js/view_passwords.js
+cp data/js/jquery-1.6.2.min.js Separate\ Addons/Reuse\ Visualizer/data/js/jquery-1.6.2.min.js
+
+# Visual Hashing (Chrome)
+cp data/js/util.js Separate\ Addons/Visual\ Hashing\ \(Chrome\)/util.js
+cp data/js/visualhash.js Separate\ Addons/Visual\ Hashing\ \(Chrome\)/visualhash.js
+cp data/lock_blue.png Separate\ Addons/Visual\ Hashing\ \(Chrome\)/lock_blue.png
